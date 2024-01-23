efb2d32ea20c04f3d8ba0ed8dc5b1a9f4fe3c570
Roberto <ramponiroberto1@gmail.com>
2024-01-23 12:13:41 +0100

    1

--- tests/steps/test.ts
+++ tests/steps/test.ts
@@ -5,7 +5,7 @@ let browser: Browser;
 let page: Page;
 
 Given('User navigates to the application',{timeout: 9000}, async function() {
-    browser= await chromium.launch({headless: false});
+    browser= await chromium.launch();
     //const browser = await chromium.launch();
     page = await browser.newPage();
     await page.goto('https://www.google.it/');
